25e46a867a52f4cea04b5a20ff25e334cc295301
jlo4 <john.love4@gmail.com>
2018-01-19 13:03:01 +0200

    up to db:migrate

--- server/models/index.js
+++ server/models/index.js
@@ -1,24 +1,19 @@
-const fs        = require('fs')
-const path      = require('path')
-const Sequelize = require('sequelize')
-const basename  = path.basename(module.filename)
-const env       = process.env.NODE_ENV || 'development'
-const config    = require(`${__dirname}/../config/config.json`)[env]
-const db        = {}
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(`${__dirname}/../config/config.json`)[env];
+const db = {};
 
-console.log(config)
-console.log(config.use_env_variable)
-let sequelize
+let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable, config.dialect)
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  console.log("nothing")
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
 }
-//else {
-//   sequelize = new Sequelize(
-//     config.database, config.username, config.password, config
-//   )
-// }
 
 fs
   .readdirSync(__dirname)
@@ -27,17 +22,17 @@ fs
     (file !== basename) &&
     (file.slice(-3) === '.js'))
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file))
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
-  })
+  });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
-    db[modelName].associate(db)
+    db[modelName].associate(db);
   }
-})
+});
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = db
+module.exports = db;
